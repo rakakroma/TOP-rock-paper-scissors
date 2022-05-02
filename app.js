bc8b66907258df7f131f8ff7f5e535f91f20d691
rakakroma <robertchun39@gmail.com>
2022-05-02 21:04:04 +0800

    finish playRound

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ function computerPlay(){
             return "Scissors";
             break;
         case 3:
-            return "Stone";
+            return "Rock";
             break;
     }
+}
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    if( player === computer ){
+        return "Tie! No one win in this Round!"
+    }
+    if( player === "paper"){
+        return computer === "scissors"? 
+        "You Lose! Scissors beats Paper": "You Win! Paper beats Rock"; 
+    }
+    if( player === "scissors"){
+        return computer === "rock"? 
+        "You Lose! Rock beats Scissors": "You Win! Scissors beats Paper"; 
+    }
+    if( player === "rock"){
+        return computer === "paper"? 
+        "You Lose! Paper beats Rock": "You Win! Rock beats Scissors"; 
+    }   
 }
